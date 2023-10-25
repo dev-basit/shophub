89ad8c85832f7029fb4e8dcd63a3185f1f3f12d1
Abdul Basit <dev.basit2020@gmail.com>
2023-10-25 08:38:15 +0500

    Product model and routes added

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 
-import data from "./constants/data.js";
-import userRouter from "./routes/users.js";
+import userRouter from "./routes/users";
+import productRouter from "./routes/product";
 
 const app = express();
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/shophub", {
@@ -14,16 +14,7 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/shophub", {
 app.get("/", (req, res) => res.send("Server is ready"));
 
 app.use("/api/users", userRouter);
-
-app.get("/api/products", (req, res) => res.send(data.products));
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((product) => product._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product not found" });
-  }
-});
+app.use("/api/products", productRouter);
 
 // middleware for handling errors
 app.use((err, req, res, next) => {
