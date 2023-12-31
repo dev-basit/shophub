23ac37713ab5a3e34b1f5d4e1ce950945ce6ae9c
Abdul Basit <dev.basit2020@gmail.com>
2023-12-31 12:18:32 +0500

    Added Register Screen

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -1,9 +1,12 @@
 import Axios from "axios";
 
-export const USER_SIGNIN_REQUEST = "USER_SIGNIN_REQUEST";
-export const USER_SIGNIN_SUCCESS = "USER_SIGNIN_SUCCESS";
-export const USER_SIGNIN_FAIL = "USER_SIGNIN_FAIL";
-export const USER_SIGNOUT = "USER_SIGNOUT";
+const USER_SIGNIN_REQUEST = "USER_SIGNIN_REQUEST";
+const USER_SIGNIN_SUCCESS = "USER_SIGNIN_SUCCESS";
+const USER_SIGNIN_FAIL = "USER_SIGNIN_FAIL";
+const USER_SIGNOUT = "USER_SIGNOUT";
+const USER_REGISTER_REQUEST = "USER_REGISTER_REQUEST";
+const USER_REGISTER_SUCCESS = "USER_REGISTER_SUCCESS";
+const USER_REGISTER_FAIL = "USER_REGISTER_FAIL";
 
 // Reducers
 export const userSigninReducer = (state = {}, action) => {
@@ -21,6 +24,19 @@ export const userSigninReducer = (state = {}, action) => {
   }
 };
 
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true };
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
 // Action  Creators
 export const signin = (email, password) => async (dispatch) => {
   dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } });
@@ -32,6 +48,24 @@ export const signin = (email, password) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: USER_SIGNIN_FAIL,
+      payload:
+        error.response && error.response.data.message ? error.response.data.message : error.message,
+    });
+  }
+};
+
+export const register = (name, email, password) => async (dispatch) => {
+  dispatch({ type: USER_REGISTER_REQUEST, payload: { email, password } });
+  try {
+    const { data } = await Axios.post("/api/users/register", { name, email, password });
+
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+    dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
       payload:
         error.response && error.response.data.message ? error.response.data.message : error.message,
     });
