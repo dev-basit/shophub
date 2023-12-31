5d48e980c632cacca031515ac21365c215e4afec
Abdul Basit <dev.basit2020@gmail.com>
2023-12-31 13:03:34 +0500

    Added shipping screen

--- frontend/src/store/cart.js
+++ frontend/src/store/cart.js
@@ -1,6 +1,8 @@
 import Axios from "axios";
 
 const CART_ADD_ITEM = "CART_ADD_ITEM";
+const CART_REMOVE_ITEM = "CART_REMOVE_ITEM";
+const CART_SAVE_SHIPPING_ADDRESS = "CART_SAVE_SHIPPING_ADDRESS";
 
 // Reducers
 export const cartReducer = (state = { cartItems: [] }, action) => {
@@ -17,6 +19,9 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
       } else {
         return { ...state, cartItems: [...state.cartItems, item] };
       }
+
+    case CART_SAVE_SHIPPING_ADDRESS:
+      return { ...state, shippingAddress: action.payload };
 
     default:
       return state;
@@ -42,4 +47,14 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
 
     localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
   } catch (error) {}
+};
+
+export const removeFromCartHandler = (productId) => (dispatch, getState) => {
+  dispatch({ type: CART_REMOVE_ITEM, payload: productId });
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const saveShippingAddress = (data) => (dispatch) => {
+  dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
+  localStorage.setItem("shippingAddress", JSON.stringify(data));
 };
